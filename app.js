b1af71c707dfc0592d8298c679ae35e4bfc1f22b
swarner2 <samuelrwarner1@gmail.com>
2016-04-02 15:18:01 -0400

    strikes and delete button
    Added strike/unstrike on click
    Added delete button on strike

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
+var addTask = function(){
+	var button = document.getElementById('addTask');
+	var input = document.getElementById('taskInput');
+	var taskList = document.getElementById('taskList');
+	var newItem = document.createElement('li');
+	var textnode = document.createTextNode(input.value);
+	newItem.class = 'task';
+	newItem.onclick = strike;
+	newItem.appendChild(textnode);
+	taskList.insertBefore(newItem, taskList.childNodes[0]);
+	input.value = '';
+};
 
 
-angular.module('myApp',['ngRoute'])
-.config(['$routeProvider',function($routeProvider){
-	$routeProvider.when('/', {
-        controller: 'homeController',
-        templateUrl: 'home.html'
-    })
-}])
-.controller('homeController', ['$scope', function($scope){
-	$scope.message = "home message";
-}])
-.controller('testController', ['$scope', function($scope){
-	$scope.foo = "lakdfj;lakfjal;dkfj";
-}])
+var strike = function(x){
+	var taskList = document.getElementById('taskList');
+	var that = this
+	var button	= document.createElement('button');
+	var text = document.createTextNode('DEL');
+	
+	button.appendChild(text);
+	button.onclick = function(){
+		console.log('button click this is: ')
+		console.log(this.parentNode.parentNode)
+		document.getElementById("taskList").removeChild(that);
+		}
+	
+	if(this.style.textDecoration === "line-through"){
+		this.style.textDecoration = "none"
+		//removes delete button
+		this.removeChild(this.childNodes[1]);
+		//move to the top of list
+		if(!del){
+			taskList.insertBefore(this, taskList.childNodes[0]);}
+	}
+	else{
+		this.style.textDecoration = "line-through";
+		this.appendChild(button);
+		//move to the end of list
+		taskList.appendChild(this)
+
+	}
+}
